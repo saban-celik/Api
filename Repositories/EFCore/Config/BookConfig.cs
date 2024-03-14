9f796ae41ac223adb5688f98f6e51d5336cf2090
Şaban Çelik <aban.elik27@gmail.com>
2024-03-14 12:08:14 +0300

    Add project files.

---
+++ Repositories/EFCore/Config/BookConfig.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "selam", Price = 5 },
+                new Book { Id = 2, Title = "selam", Price = 5 },
+                new Book { Id = 3, Title = "selam", Price = 5 }
+                );
+        }
+    }
+}
